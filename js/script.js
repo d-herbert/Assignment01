aa064b00a02e91e2db39b52eef85f3db2c2f882e
d-herbert <deidre.m.herbert@gmail.com>
2022-02-21 20:59:27 -0800

    final submit

---
+++ js/script.js
@@ -0,0 +1,123 @@
+// // STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// // STEP 2
+// 4.4;  // numeric literal 
+// 'Hello World!';  // string literal 
+// false;  // boolean literal 
+// null;  // null literal value 
+
+// // STEP 3
+// let expressionA = 3 * (4 / 5) + 6; 
+// let expressionB = 'Hello' + ' ' + 'World';
+
+// // STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let strState // Hungarian notation
+// let zipCode
+// let yourAge
+// let referralSource
+// let mayWeContactYou
+
+// // STEP 5
+// let firstName = 'Deidre';
+// let zipCode = 12345;
+// let mayWeContactYou = false;
+
+// // STEP 6
+// let firstName = 'Deidre';
+// let zipCode = 12345;
+// let displayValue1 = 'Testing string + number: ' + '\n' + firstName + zipCode;
+// console.log(displayValue1);
+
+// // STEP 7
+// let mayWeContactYou = false;
+// let firstName = 'Deidre';
+// let zipCode = 12345;
+// let displayValue2 = 'Testing boolean + string: ' + '\n' + mayWeContactYou + firstName;
+// console.log(displayValue2);
+// let displayValue3 = 'Testing number + boolean: ' + '\n' + zipCode + mayWeContactYou;
+// console.log(displayValue3);
+
+// // STEP 8
+// // No, here's the edited string:
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`;
+// window.console.log(someString);
+
+// // STEP 9
+// let returnNull = null;// returns Null
+// let returnNan = 32 / 'cat';// returns NaN / undefined
+// console.log(returnNull);
+// console.log(returnNan);
+
+// // STEP 10
+// let ob = new Object();  
+// console.log(typeof 'cat', typeof 4, typeof false, typeof ob, typeof 1/0);
+
+// // STEP 11
+// returnHello = 'Hello ' + 'Deidre Herbert' + ', welcome to the JavaScript class!';
+// alert(returnHello);
+
+// // STEP 12
+// let myName = 'Deidre Herbert';
+// returnHello = 'Hello ' + myName + ', welcome to the JavaScript class!';
+// alert(returnHello);
+
+// // STEP 13
+// let myName = 'Deidre Herbert';
+// let course = 'JavaScript';
+// returnMessage = 'Hello ' + myName + ', welcome to the ' + course + ' class!';
+// alert(returnMessage);
+
+// // STEP 14
+// let course = 'JavaScript';
+// let myName = 'Deidre Herbert';
+// returnMessageBreak = 'Hello ' + myName + '.' + '\n' + 'Welcome to the ' + course + ' class!';
+// alert(returnMessageBreak);
+
+
+// // STEP 15
+// let course = 'JavaScript';
+// let askName = prompt('Enter your name: ');
+// returnMessageInput = 'Hello ' + askName + '.' + '\n' + 'Welcome to the ' + course + ' class!';
+// alert(returnMessageInput);
+
+// // STEP 16
+// let askName = prompt('Enter your name: ');
+// let askCourse = prompt('Enter your class name: ');
+// returnNameClass = 'Hello ' + askName + '.' + '\n' + 'Welcome to the ' + askCourse + ' class!';
+// alert(returnNameClass);
+
+// // STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// // STEP 18
+// let x = 20;
+// x += x;
+// console.log(x);
+
+// // STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// // STEP 20
+// let x = 20 % 3;
+// console.log(x);
+
+// // STEP 21
+// let x = 5;
+// console.log(x === 5);
+
+// // STEP 22
+// let x = 3;
+// console.log(x > 5);
